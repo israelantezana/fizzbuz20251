d6ff78062dc679b6bcdbceae286b23155630d100
Israel Antezana Rojas <israelantezana@gmail.com>
2025-02-17 23:41:22 -0400

    refact: se refactoring la comparion

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -7,9 +7,13 @@ describe("Fizzbuzz", () => {
   it("genera Fizz si el numero es 3", () => {
     expect(fizzbuzz(3)).toEqual("Fizz");
   });
+  it("genera Buzz si el numero es 5", () => {
+    expect(fizzbuzz(5)).toEqual("Buzz");
+  });
 });
 
 function fizzbuzz(n) {
-  if (n === 1) return "1";
-  return "Fizz";
+  if (n == 1) return "1";
+  if (n == 3) return "Fizz";
+  return "Buzz";
 }
